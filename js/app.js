bc20a882051b04b0214b0f34d2d358bd10cf1bd6
lawrence33 <lclaytoniii@gmail.com>
2016-10-17 15:36:00 -0500

    spotify almost there.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Globals
 var base = 'https://api.spotify.com';
+var songID = '/v1/tracks/{id}';
 var songSearch = '/v1/search?type=track';
 console.log(base + songSearch);
 
@@ -20,11 +21,43 @@ var tabData = function(query) {
       data: params,
       success: function(data) {
         var tracks = data.tracks.items;
+        // console.log(tracks, 'tracks')
         $.each(tracks, function(i, items){
-        console.log(items.artists);
-        });
+        console.log(items, 'artists')
+          var songPost = clonePost(items);
+      $('.song-results').append(songPost);
+      });
       }
     });
+};
+
+//clone the template, post data to template & append to 'song-results'
+var clonePost = function(artistsData){
+
+  var newPost = $('.template .addArtist').clone();
+        // console.log(newPost, 'clone')
+
+  var newArtist = newPost.find('.nameArtist');
+  newArtist.text(artistsData.artists[0].name);
+  // console.log(artistsData.artists[0].name, 'name');
+
+  var albumImg = newPost.find('.addImg');
+  // var albumCover = artistsData.album.images[].url;
+  // console.log(artistsData.album.images[].url, 'img');
+  albumImg.html('<img src=' + "artistsData.album.images[].url"+ '/>');
+
+  var songLink = newPost.find('.songLink');
+  // songLink.html("<embed src="+ 'artistsData.uri' +">''</embed>");
+  songLink.html('<iframe src="https://embed.spotify.com/?uri='
+  + 'artistsData.uri"' 
+  + 'width="150" + height="180" frameborder="0"' 
+  + 'allowtransparency="true">' + '</iframe>');
+
+  // songLink.html<'a href="' + 'https://api.spotify.com/v1/tracks/1gLOB2DBPvG1Jrupdj9PRc' + '"></a>';
+  // console.log(artistsData.uri, 'song');
+
+  return newPost;
+
 };
 
     $(function() {
@@ -34,7 +67,6 @@ var tabData = function(query) {
 
         // var song = $(this).find("input[name='songTitle']").val();
         var song = $('.songTitle').val();
-        console.log(song, 'song');
 
         tabData(song);
 
