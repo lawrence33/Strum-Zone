641ea731ef623c8298404fa507238bf6706038ad
lawrence33 <lclaytoniii@gmail.com>
2016-10-18 22:39:04 -0500

    spotify works ok, songster up & running

--- js/app.js
+++ js/app.js
@@ -5,64 +5,64 @@ var songSearch = '/v1/search?type=track';
 var urlSong = 'http://www.songsterr.com/a/ra/songs.json?pattern='
 
 
-// var tabData = function(query) {
-//     // console.log(query, 'que');
+var tabData = function(query) {
+    // console.log(query, 'que');
 
-//     var params = {
-//        q: query,
-//      type: 'track',
-//       client_id: '263dae5b8ae14bae8e71e20dc4e97afe',
-//       client_secret: '35be8278b6a0481da43b5eb6042a28db'
-//     };
+    var params = {
+       q: query,
+     type: 'track',
+      client_id: '263dae5b8ae14bae8e71e20dc4e97afe',
+      client_secret: '35be8278b6a0481da43b5eb6042a28db'
+    };
 
-//     $.ajax({
-//       type: 'GET',
-//       url: base + songSearch,
-//       datatype: 'json',
-//       data: params,
-//       success: function(data) {
-//         var tracks = data.tracks.items;
-//         // console.log(tracks, 'tracks')
-//         $.each(tracks, function(i, items){
-//         console.log(items, 'artists')
-//           var songPost = clonePost(items);
-//       $('.song-results').append(songPost);
-//       $('.go-to-tabs').removeClass();
-//       });
-//       }
-//     });
+    $.ajax({
+      type: 'GET',
+      url: base + songSearch,
+      datatype: 'json',
+      data: params,
+      success: function(data) {
+        var tracks = data.tracks.items;
+        // console.log(tracks, 'tracks')
+        $.each(tracks, function(i, items){
+        console.log(items, 'artists')
+          var songPost = clonePost(items);
+      $('.song-results').append(songPost);
+      $('.go-to-tabs').removeClass();
+      });
+      }
+    });
 
-// };
+};
 
-// //clone the template, post data to template & append to 'song-results'
-// var clonePost = function(artistsData){
+//clone the template, post data to template & append to 'song-results'
+var clonePost = function(artistsData){
 
-//   var newPost = $('.template .addArtist').clone();
-//         // console.log(newPost, 'clone')
+  var newPost = $('.template .addArtist').clone();
+        // console.log(newPost, 'clone')
 
-//   var newArtist = newPost.find('.nameArtist');
-//   newArtist.text(artistsData.artists[0].name);
-//   // console.log(artistsData.artists[0].name, 'name');
+  var newArtist = newPost.find('.nameArtist');
+  newArtist.text(artistsData.artists[0].name);
+  // console.log(artistsData.artists[0].name, 'name');
 
-//   var albumImg = newPost.find('.addImg');
-//   // var albumCover = artistsData.album.images[].url;
-//   // console.log(artistsData.album.images[].url, 'img');
-//   albumImg.html('<img src=' + "artistsData.album.images[].url"+ '/>');
+  var albumImg = newPost.find('.addImg');
+  // var albumCover = artistsData.album.images[].url;
+  // console.log(artistsData.album.images[].url, 'img');
+  albumImg.html('<img src=' + "artistsData.album.images[].url"+ '/>');
 
-//   var songLink = newPost.find('.songLink');
-//   var uri = artistsData.uri;
-//   // songLink.html("<embed src="+ 'artistsData.uri' +">''</embed>");
-//   songLink.html('<iframe src="https://embed.spotify.com/?uri='
-//   + uri + '"' 
-//   + 'width="200" + height="230" frameborder="0"' 
-//   + 'allowtransparency="true">' + '</iframe>');
+  var songLink = newPost.find('.songLink');
+  var uri = artistsData.uri;
+  // songLink.html("<embed src="+ 'artistsData.uri' +">''</embed>");
+  songLink.html('<iframe src="https://embed.spotify.com/?uri='
+  + uri + '"' 
+  + 'width="200" + height="230" frameborder="0"' 
+  + 'allowtransparency="true">' + '</iframe>');
 
-//   // songLink.html<'a href="' + 'https://api.spotify.com/v1/tracks/1gLOB2DBPvG1Jrupdj9PRc' + '"></a>';
-//   // console.log(artistsData.uri, 'song');
+  // songLink.html<'a href="' + 'https://api.spotify.com/v1/tracks/1gLOB2DBPvG1Jrupdj9PRc' + '"></a>';
+  // console.log(artistsData.uri, 'song');
 
-//   return newPost;
+  return newPost;
 
-// };
+};
 
 var getTabs = function(songName){
   
@@ -97,7 +97,7 @@ var getTabs = function(songName){
         e.preventDefault();
 
         var song = $('.songTitle').val();
-        // tabData(song);
+        tabData(song);
 
         getTabs(song);
       });
